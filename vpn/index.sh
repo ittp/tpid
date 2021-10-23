f135e99adcd65e8a6a4e6b5d0dfd39db1cd4e543
ittp <61617660+ittp@users.noreply.github.com>
2021-10-23 16:26:15 +0300

    Update index.sh

--- vpn/index.sh
+++ vpn/index.sh
@@ -31,3 +31,32 @@ echo "CL"
 
   cat ${CLIENT_KEY}
 }
+
+
+config() {
+
+CMD="--local 0.0.0.0 --server 10.10.10.0 255.255.255.0 --port 1194  --tls-auth /etc/openvpn/tc.key 0 --cipher AES-256-CBC --data-ciphers AES-256-CBC --push "redirect-gateway def1 bypass-dhcp"  --proto udp --dev tun2 --dev-type tun --topology subnet --ca /etc/openvpn/ca.crt --cert /etc/openvpn/server.crt --key     /etc/openvpn/server.key --dh      /etc/openvpn/dh.pem --push 'route 192.168.1.0 255.255.255.0' --push 'route 10.10.10.0 255.255.255.0'"
+
+
+
+
+  # internal tun0 connection IP
+ifconfig-pool-persist ipp.txt
+
+
+push "route 10.10.10.0 255.255.255.0"
+
+push "redirect-gateway def1 bypass-dhcp"
+
+keepalive 10 120
+tls-auth /etc/openvpn/tc.key 0
+auth-nocache
+cipher AES-256-CBC
+data-ciphers AES-256-CBC
+persist-key
+persist-tun
+status /etc/openvpn/openvpn-status.log
+verb 5  # verbose mode
+client-to-client
+explicit-exit-notify 1"
+}
